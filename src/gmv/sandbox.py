8af510b18a3f0f8298f9a992bffdccc9aee2c8c2
Guillaume Aubert <guillaume.aubert@gmail.com>
2012-01-30 16:24:04 +0000

    add sandbox file

---
+++ src/gmv/sandbox.py
@@ -0,0 +1,14 @@
+'''
+Created on Jan 30, 2012
+
+@author: guillaume.aubert@gmail.com
+'''
+
+from cmdline_utils  import CmdLineParser
+
+if __name__ == '__main__':
+    
+    global_parser = CmdLineParser()
+    global_parser.disable_interspersed_args()
+    
+    
