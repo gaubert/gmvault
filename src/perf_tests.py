371cadf084266b2658534ebb4edc1572cadb21d3
Guillaume Aubert <guillaume.aubert@gmail.com>
2012-03-28 14:47:26 +0000

    add GPLV3 licensing

--- src/perf_tests.py
+++ src/perf_tests.py
@@ -1,8 +1,21 @@
 '''
-Created on Feb 8, 2012
+    Gmvault: a tool to backup and restore your gmail account.
+    Copyright (C) <2011-2012>  <guillaume Aubert (guillaume dot aubert at gmail do com)>
 
-@author: guillaume.aubert@gmail.com
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
 import unittest
 import datetime
 import os
