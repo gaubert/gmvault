7e8dca18c1e0b4dd0a0b4832ea7948f756ecf862
Guillaume Aubert <guillaume.aubert@gmail.com>
2012-06-26 13:21:53 +0000

    optimize deletion by not creating a sub set systematically and fetching more ids

--- src/gmv/gmvault.py
+++ src/gmv/gmvault.py
@@ -819,16 +819,21 @@ class GMVaulter(object):
             #query nb_items items in one query to minimise number of imap queries
             for group_imap_id in itertools.izip_longest(fillvalue=None, *[iter(imap_ids)]*nb_items):
                 
+                timer2.start()
                 # if None in list remove it
                 if None in group_imap_id: 
                     group_imap_id = [ im_id for im_id in group_imap_id if im_id != None ]
+                LOG.critical("list comprehension Elapsed time = %s sec" % (timer2.elapsed_ms()))
                 
-                data = self.src.fetch(group_imap_id, imap_utils.GIMAPFetcher.GET_GMAIL_ID)
-                
-                #for key in data:
-                #    db_gmail_ids.discard(data[key][imap_utils.GIMAPFetcher.GMAIL_ID])
                 timer2.start()
-                db_gmail_ids.difference_update({ data[key][imap_utils.GIMAPFetcher.GMAIL_ID] for key in data })
+                data = self.src.fetch(group_imap_id, imap_utils.GIMAPFetcher.GET_GMAIL_ID)
+                LOG.critical("fetching time = %s sec" % (timer2.elapsed_ms()))
+                
+                timer2.start()
+                for key in data:
+                    db_gmail_ids.discard(data[key][imap_utils.GIMAPFetcher.GMAIL_ID])
+                
+                #db_gmail_ids.difference_update({ data[key][imap_utils.GIMAPFetcher.GMAIL_ID] for key in data })
                 LOG.critical("Difference Elapsed time = %s sec" % (timer2.elapsed_ms()))
                 
                 #quit loop if db set is already empty
