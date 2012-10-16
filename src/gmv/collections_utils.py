5524a9bd83fc26a858a05cafb028fbf0678c2ccd
Guillaume Aubert <guillaume.aubert@gmail.com>
2012-10-16 18:36:55 +0300

    add multimap

--- src/gmv/collections_utils.py
+++ src/gmv/collections_utils.py
@@ -86,5 +86,69 @@ class OrderedDict(dict, MutableMapping):
         return d
 ## end of http://code.activestate.com/recipes/576669/ }}}
 
+import collections
+import sets
+
+class Map(object):
+    """ Map wraps a dictionary. It is essentially an abstract class from which
+    specific multimaps are subclassed. """
+    def __init__(self):
+        self._dict = {}
+        
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, repr(self._dict))
+    
+    __str__ = __repr__
+        
+    def __getitem__(self, key):
+        return self._dict[key]
+    
+    def __setitem__(self, key, value):
+        self._dict[key] = value
+    
+    def __delitem__(self, key):
+        del self._dict[key]
+        
+    def remove(self, key, value):
+        del self._dict[key]
+    
+    def dict(self):
+        """ Allows access to internal dictionary, if necessary. Caution: multimaps 
+        will break if keys are not associated with proper container."""
+        return self._dict
+
+class ListMultimap(Map):
+    """ ListMultimap is based on lists and allows multiple instances of same value. """
+    def __init__(self):
+        self._dict = collections.defaultdict(list)
+        
+    def __setitem__(self, key, value):
+        self._dict[key].append(value)
+    
+    def remove(self, key, value):
+        self._dict[key].remove(value)
+
+class SetMultimap(Map):
+    """ SetMultimap is based on sets and prevents multiple instances of same value. """
+    def __init__(self):
+        self._dict = collections.defaultdict(sets.Set)
+        
+    def __setitem__(self, key, value):
+        self._dict[key].add(value)
+    
+    def remove(self, key, value):
+        self._dict[key].remove(value)
+
+class DictMultimap(Map):
+    """ DictMultimap is based on dicts and allows fast tests for membership. """
+    def __init__(self):
+        self._dict = collections.defaultdict(dict)
+        
+    def __setitem__(self, key, value):
+        self._dict[key][value] = True
+    
+    def remove(self, key, value):
+        del self._dict[key][value]
+
 
 
