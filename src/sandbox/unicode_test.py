031dc28f62083716df82bd0397e488f7fcc3d2c4
Guillaume Aubert <guillaume.aubert@gmail.com>
2013-01-14 21:11:37 +0100

    work on unicode_test.py

--- src/sandbox/unicode_test.py
+++ src/sandbox/unicode_test.py
@@ -11,15 +11,21 @@ def ascii_hex(str):
 def convert_to_utf8(a_str):
     """
     """
-    #import chardet
-    #char_enc = chardet.detect(a_str)
-    #print("detected encoding = %s" % (char_enc))
-    #print("system machine encoding = %s" % (sys.getdefaultencoding()))
-    #u_str = unicode(a_str, char_enc['encoding'], errors='ignore')
-    u_str = a_str
+    if type(a_str) != type(u'a'):
+		import chardet
+		char_enc = chardet.detect(a_str)
+		print("detected encoding = %s" % (char_enc))
+		print("system machine encoding = %s" % (sys.getdefaultencoding()))
+		#u_str = unicode(a_str, char_enc['encoding'], errors='ignore')
+		u_str = unicode(a_str, 'utf-8', errors='ignore')
+    else:
+        print("Already unicode do not convert")
+        u_str = a_str
+
+    print("raw unicode = %s" % (u_str))
     #u_str = unicodedata.normalize('NFKC',u_str)
     u_str = u_str.encode('unicode_escape').decode('unicode_escape')
-    print("raw unicode = %s" % (u_str))
+    print("unicode escape = %s" % (u_str))
     print("normalized unicode(NFKD) = %s" % (repr(unicodedata.normalize('NFKD',u_str))))
     print("normalized unicode(NFKC) = %s" % (repr(unicodedata.normalize('NFKC',u_str))))
     print("normalized unicode(NFC) = %s" % (repr(unicodedata.normalize('NFC',u_str))))
@@ -34,5 +40,5 @@ def convert_to_utf8(a_str):
 if __name__ == '__main__':
 
    u_str = u"label:èévader"
-   #convert_to_utf8(sys.argv[1])
-   convert_to_utf8(u_str)
+   convert_to_utf8(sys.argv[1])
+   #convert_to_utf8(u_str)
