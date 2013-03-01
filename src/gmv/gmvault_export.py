cb76d415db1843c498892576168af0e2df8acebd
Guillaume Aubert <guillaume.aubert@gmail.com>
2013-03-01 15:25:12 +0100

    pylinting gmvault_export

--- src/gmv/gmvault_export.py
+++ src/gmv/gmvault_export.py
@@ -15,6 +15,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+'''
+   Export function of Gmvault created by dave@vasilevsky.ca
+'''
 
 import os
 import re
